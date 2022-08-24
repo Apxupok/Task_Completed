d92b547cf54573d158e5098762488e8f236669b5
Apxupok <apxupok@gmail.com>
2022-08-24 07:55:52 +0500

    Correct

--- app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -5,18 +5,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,21 +41,24 @@ class MainActivity : ComponentActivity() {
 fun NiceDone(tasks: String= stringResource(R.string.tasks) ,
              wellDone: String= stringResource(R.string.well_done),
              image:Painter = painterResource(R.drawable.ic_task_completed),
-             modifier:Modifier = Modifier) {
+             modifier:Modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
 
-    Column(modifier) {
-
+    Column(modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Image(painter = image, contentDescription = null)
-        Text(text = tasks,
+
+        Text(
+            text = tasks,
             textAlign = TextAlign.Center,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
         )
 
-        Text(text = wellDone,
-            textAlign = TextAlign.Center,
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
+        Text(
+            text = wellDone,
+            fontSize = 16.sp
         )
     }
 }
